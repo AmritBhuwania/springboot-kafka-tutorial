333e51d6ed2c2dbdd1b7ce206c199420b289a1d2
abhuwania <abhuwania@zipcar.com>
2022-07-02 20:04:13 +0530

    add my files

--- src/main/java/com/amrit/springboot/SpringbootKafkaTutorialApplication.java
+++ src/main/java/com/amrit/springboot/SpringbootKafkaTutorialApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootKafkaTutorialApplication {
 
+	// Refer: https://youtube.com/playlist?list=PLGRDMO4rOGcNLwoack4ZiTyewUcF6y6BU
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootKafkaTutorialApplication.class, args);
 	}
